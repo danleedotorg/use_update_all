f02d9a17ea8d40d26e15e8fb5426d8697b88bd7e
Daniel Lee <danlee1996@proton.me>
2023-12-25 20:58:49 -0700

    changing project name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "avoid_active_record_production"
+  # config.active_job.queue_name_prefix = "use_update_all_production"
 
   config.action_mailer.perform_caching = false
 
